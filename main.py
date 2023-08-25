36d28c2e8afe169b3accf3d908c96a84461eddee
oink <oink@oink.com>
2023-08-25 22:44:59 +0800

    update - 1

---
+++ main.py
@@ -0,0 +1,38 @@
+import random
+
+def getRandomNumber(a, b):
+    return random.randint(a, b)
+
+
+ERROR = '_'
+CORRECT = 'O'
+WRONG_SPOT = 'X'
+randNumber = None
+
+while True:
+    randNumber = str(getRandomNumber(1000, 9999))
+    print("rand: ", randNumber)
+    count = 0
+    while True:
+        guess = input("Please enter the guessed number ('q' indicates exit):")
+        if guess.upper() == 'Q':
+            break
+        count+=1
+        while len(guess) < len(randNumber):
+            guess += '_'
+        guess_array = [ERROR for i in range(len(randNumber))]
+        for i in range(len(randNumber)):
+            if guess[i] == randNumber[i]:
+                guess_array[i] = CORRECT
+            elif guess[i] in randNumber:
+                guess_array[i] = WRONG_SPOT
+
+        print(guess_array)
+        if ERROR not in guess_array and WRONG_SPOT not in guess_array:
+            print("Congratulations, you guessed it right!")
+
+    print("Try {} times in total".format(count))
+
+    op = input("Continue (Y/N):")
+    if op.upper() != 'Y':break
+
