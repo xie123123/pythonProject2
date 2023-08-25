2a95aaf6a765eadd714e0d42ef0bcac6d56849eb
oink <oink@oink.com>
2023-08-25 22:46:16 +0800

    update - 3

--- main.py
+++ main.py
@@ -1,44 +1,49 @@
+"""Import random package"""
 import random
 
 
-def getRandomNumber(a, b):
-    return random.randint(a, b)
+def get_random_number(left, right):
+    """Returns a random number between [left, right]"""
+    return random.randint(left, right)
 
 
 ERROR = '_'
 CORRECT = 'O'
 WRONG_SPOT = 'X'
-randNumber = None
+RANDNUMBER = None
 
 
-def getResult(randNumber, guess):
-    while len(guess) < len(randNumber):
-        guess += '_'
-    guess_array = [ERROR for i in range(len(randNumber))]
-    for i in range(len(randNumber)):
-        if guess[i] == randNumber[i]:
-            guess_array[i] = CORRECT
-        elif guess[i] in randNumber:
-            guess_array[i] = WRONG_SPOT
-    return guess_array
+def get_result(rand_number, guess_):
+    """Returns the matching result between the guessed
+    number and the actual number"""
+    while len(guess_) < len(rand_number):
+        guess_ += '_'
+    guess_array_ = [ERROR for i in range(len(rand_number))]
+
+    for i in enumerate(rand_number):
+        if guess_[i] == rand_number[i]:
+            guess_array_[i] = CORRECT
+        elif guess_[i] in rand_number:
+            guess_array_[i] = WRONG_SPOT
+    return guess_array_
 
 
 while True:
-    randNumber = str(getRandomNumber(1000, 9999))
-    print("rand: ", randNumber)
-    count = 0
+    RANDNUMBER = str(get_random_number(1000, 9999))
+    print("rand: ", RANDNUMBER)
+    COUNT = 0
     while True:
         guess = input("Please enter the guessed number ('q' indicates exit):")
         if guess.upper() == 'Q':
             break
-        count += 1
-        guess_array = getResult(randNumber, guess)
+        COUNT += 1
+        guess_array = get_result(RANDNUMBER, guess)
 
         print(guess_array)
         if ERROR not in guess_array and WRONG_SPOT not in guess_array:
             print("Congratulations, you guessed it right!")
 
-    print("Try {} times in total".format(count))
+    print(f"Try {COUNT} times in total")
 
     op = input("Continue (Y/N):")
     if op.upper() != 'Y':
