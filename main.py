f930ca56d14d126e973905360c8b8e6b7ed7ec44
oink <oink@oink.com>
2023-08-25 22:45:53 +0800

    update - 2

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import random
 
+
 def getRandomNumber(a, b):
     return random.randint(a, b)
 
@@ -9,6 +10,19 @@ CORRECT = 'O'
 WRONG_SPOT = 'X'
 randNumber = None
 
+
+def getResult(randNumber, guess):
+    while len(guess) < len(randNumber):
+        guess += '_'
+    guess_array = [ERROR for i in range(len(randNumber))]
+    for i in range(len(randNumber)):
+        if guess[i] == randNumber[i]:
+            guess_array[i] = CORRECT
+        elif guess[i] in randNumber:
+            guess_array[i] = WRONG_SPOT
+    return guess_array
+
+
 while True:
     randNumber = str(getRandomNumber(1000, 9999))
     print("rand: ", randNumber)
@@ -17,15 +31,8 @@ while True:
         guess = input("Please enter the guessed number ('q' indicates exit):")
         if guess.upper() == 'Q':
             break
-        count+=1
-        while len(guess) < len(randNumber):
-            guess += '_'
-        guess_array = [ERROR for i in range(len(randNumber))]
-        for i in range(len(randNumber)):
-            if guess[i] == randNumber[i]:
-                guess_array[i] = CORRECT
-            elif guess[i] in randNumber:
-                guess_array[i] = WRONG_SPOT
+        count += 1
+        guess_array = getResult(randNumber, guess)
 
         print(guess_array)
         if ERROR not in guess_array and WRONG_SPOT not in guess_array:
@@ -34,5 +41,5 @@ while True:
     print("Try {} times in total".format(count))
 
     op = input("Continue (Y/N):")
-    if op.upper() != 'Y':break
-
+    if op.upper() != 'Y':
+        break
